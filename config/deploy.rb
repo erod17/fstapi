ccaec8eca88fee4f0a1464187e652ef3caf6090f
Erik Rodriguez <erod17@gmail.com>
2019-09-28 05:13:51 -0400

    Update Deploy 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.1"
 
 set :application, "fstapi"
-set :repo_url, "https://github.com/erod17/fstapi.git"
+set :repo_url, "git://github.com/erod17/fstapi.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
